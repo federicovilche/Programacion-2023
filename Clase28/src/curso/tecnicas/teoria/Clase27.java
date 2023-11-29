cff6a2894cd1a537909f8e5ac8e0c17d608de770
Federico Nahuel Vilche <143443840+federicovilche@users.noreply.github.com>
2023-11-29 13:29:12 -0300

    Add files via upload

---
+++ Clase28/src/curso/tecnicas/teoria/Clase27.java
@@ -0,0 +1,33 @@
+
+package curso.tecnicas.teoria;
+
+
+public class Clase27 {
+
+
+    public static void main(String[] args) {
+   /*
+        ANT (Abstract Windows Toolkit) - SWING - JavaFX
+        
+        En Java existen muchas interfacea gráficas y se pueden agregar más de
+        forma externa.
+        La primera en aparecer fue AWT. Era un interfaz gráfica muy sencilla y 
+        veloz, estaba incluida dentro de la librería standard en Java. Pero el
+        mayor problema lo tenía en lo que se veía igual en todos los SO.
+        Con el tiempo apareció SWING, que utiliza AWT pero garantiza que su aspecto 
+        sea igual en todos en todos SO.
+        A partir del JDK 5 aparece Java FX, que a partir de la versión JDK 7 comenzó
+        a formar parte integral del nucleo de Java. A partir de la versión JDK 10
+        ya no se incluye 
+        
+        */
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
